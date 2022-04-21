8fe7ea73055051d0dbcb042a3de92ed83ea16236
Archanagowda23 <archanar249@gmail.com>
2022-04-21 20:31:58 +0530

    initial commit

---
+++ src/main/java/EmployeeDatabase.java
@@ -0,0 +1,11 @@
+import java.sql.*;
+
+public class EmployeeDatabase {
+
+    public static void main(String[] args) {
+        System.out.println("welcome to employee-jdbc program");
+    }
+}
+
+
+
